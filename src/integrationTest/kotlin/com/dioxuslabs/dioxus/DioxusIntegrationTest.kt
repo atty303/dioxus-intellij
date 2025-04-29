eb0a4087ea2cc2bb4360a35d1df6a82dc4e558b4
Koji AGAWA <atty303@gmail.com>
2025-04-29 21:16:41 +0900

    Revert "test: integration test"
    This reverts commit def4000e058205e7fde5866c3f430b02de3b7a91.

--- src/integrationTest/kotlin/com/dioxuslabs/dioxus/DioxusIntegrationTest.kt
+++ src/integrationTest/kotlin/com/dioxuslabs/dioxus/DioxusIntegrationTest.kt
@@ -1,11 +1,13 @@
 package com.dioxuslabs.dioxus
 
+import com.dioxuslabs.dioxus.actions.DioxusCheckOnSaveAction
 import com.intellij.driver.sdk.invokeAction
 import com.intellij.driver.sdk.ui.components.common.codeEditor
 import com.intellij.driver.sdk.ui.components.common.editor
 import com.intellij.driver.sdk.ui.components.common.ideFrame
-import com.intellij.driver.sdk.ui.components.common.invokeOpenFileAction
-import com.intellij.driver.sdk.ui.components.common.popups.searchEverywherePopup
+import com.intellij.driver.sdk.ui.components.elements.dialog
+import com.intellij.driver.sdk.ui.components.elements.tree
+
 import com.intellij.driver.sdk.waitForIndicators
 import com.intellij.framework.detection.FileContentPattern.fileContent
 import com.intellij.ide.starter.ci.CIServer
@@ -15,14 +17,28 @@ import com.intellij.ide.starter.driver.engine.runIdeWithDriver
 import com.intellij.ide.starter.ide.IdeProductProvider
 import com.intellij.ide.starter.models.TestCase
 import com.intellij.ide.starter.plugins.PluginConfigurator
+import com.intellij.ide.starter.project.GitHubProject
 import com.intellij.ide.starter.project.LocalProjectInfo
+import com.intellij.ide.starter.report.sarif.driver
 import com.intellij.ide.starter.runner.Starter
+import com.intellij.openapi.actionSystem.ex.ActionUtil.invokeAction
+import com.intellij.openapi.application.ReadAction
+
+import com.intellij.openapi.application.runWriteAction
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiManager
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.fail
 import org.kodein.di.DI
 import org.kodein.di.bindSingleton
 import java.io.File
 import java.nio.file.Files
+import java.nio.file.Paths
 import kotlin.io.path.Path
 import kotlin.time.Duration.Companion.minutes
 
@@ -54,27 +70,36 @@ class PluginTest {
             ).withVersion("2025.1")
         ).apply {
             val pathToPlugin = System.getProperty("path.to.build.plugin")
-            PluginConfigurator(this).installPluginFromPluginManager("com.jetbrains.rust", "251.23774.463")
             PluginConfigurator(this).installPluginFromFolder(File(pathToPlugin))
         }.runIdeWithDriver().useDriverAndCloseIde {
             waitForIndicators(5.minutes)
 //            Thread.sleep(30.minutes.inWholeMilliseconds)
             ideFrame {
-                invokeAction("SearchEverywhere")
-                searchEverywherePopup {
-                    searchAndChooseFirst("main.rs")
-                }
+                invokeAction("ShowSettings")
+
+                editor("src/main.rs")
                 codeEditor {
-//                    goToPosition(0, 0)
-//                    keyboard { space() }
-                    keyboard { space() }
+
                     invokeAction("SaveAll")
                     waitForIndicators(1.minutes)
                 }
 
                 val fileContent = Files.readString(Path("src/integrationTest/resources/rust-project/src/main.rs"))
-                System.err.println("取得したファイルの内容:\n$fileContent")
+                println("取得したファイルの内容:\n$fileContent")
             }
+        }
+    }
+}
+
+class BackendService {
+    fun getFileContent(projectBasePath: String, relativePath: String): String {
+        val filePath = Paths.get(projectBasePath, relativePath).toString()
+        // ReadAction を使用して VFS から安全にファイルを取得
+        return ReadAction.compute<String, Throwable> {
+            val virtualFile = LocalFileSystem.getInstance().findFileByPath(filePath)
+                ?: throw IllegalStateException("File not found: $filePath")
+            // VfsUtil を使ってファイルの内容を読み込む
+            VfsUtil.loadText(virtualFile)
         }
     }
 }
